5d380e6539593fc9f17a047a4d0e308d23b139eb
gededrm <114561837+gede-drm@users.noreply.github.com>
2023-02-28 16:59:17 +0700

    Initiate Login page

---
+++ app/src/main/java/com/geded/anative160420008/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.geded.anative160420008
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //todo: Buat Halaman Login
+    }
+}
